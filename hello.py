d0424f0d61fd04056e95a399c9797745d5acef56
Miguel Palomera <mpalomera@users.noreply.github.com>
2018-08-06 09:43:12 -0500

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,6 @@
+"""
+All you have to do is type return "hello edabit.com" on the second line and click the Check button. 
+If everything went according to plan, the button will turn red and say SUBMIT FINAL. Click it and see what happens.
+"""
+def hello():
+	return "hello"
